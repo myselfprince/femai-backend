1c5060ea1fdf0ace968db95a34361d269dd80e15
myselfprince <contactprincemedia@gmail.com>
2025-03-16 13:45:54 +0530

    fixed more errors

--- app.py
+++ app.py
@@ -111,6 +111,6 @@ async def predict(data: HealthInput):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
